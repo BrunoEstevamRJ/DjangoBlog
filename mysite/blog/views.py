e4b93c9cef7dfa11344564e13852d0cba4491bc3
BrunoEstevamRJ <bruno.estevam@outlook.com.br>
2024-10-18 22:32:47 -0300

    user_post template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from django.views import generic
 
