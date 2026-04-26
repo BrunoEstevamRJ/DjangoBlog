7ce3ca164836492dbc708e352237a3a54a0477de
Bruno Estevam <bruno.estevam@outlook.com.br>
2026-04-26 12:42:58 -0300

    feat: add people discovery and profile post visibility

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -4,6 +4,7 @@ from accounts import views as accounts_views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('people/', accounts_views.discover_people, name='discover_people'),
     path('profile/<str:username>/', accounts_views.profile, name='profile'),
     path('edit-profile/', accounts_views.edit_profile, name='edit_profile'),
     path('profile/<str:username>/follow/', accounts_views.toggle_follow, name='toggle_follow'),
