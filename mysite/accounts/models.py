34a37fb640acd2ecc6196a5589f5d0522efc7efa
Bruno Estevam <67297226+BrunoEstevamRJ@users.noreply.github.com>
2026-04-26 12:01:03 -0300

    Add follow relationships and filter home timeline by following

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -9,6 +9,12 @@ class Profile(models.Model):
     profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
+    following = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="followers",
+        blank=True,
+    )
 
     def __str__(self):
         return self.user.username
